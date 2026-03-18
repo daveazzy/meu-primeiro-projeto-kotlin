d36f984008d1f60a9b789fb6c1714ccdd5b330ea
daveazzy <davidsonoliveira47@gmail.com>
2026-03-18 11:29:33 -0300

    frist commit

---
+++ src/aula_01_mutabilidade_imutabilidade/01_ValVar.kt
@@ -0,0 +1,13 @@
+package aula_01_mutabilidade_imutabilidade
+
+fun main() {
+
+    // val = value (não pode ser alterado)
+    val name = "Davidson Oliveira"
+
+    // var pode mudar
+    var age = 27
+    age = 28
+
+    // age = "Davidson" => tipos imcompatíveis. nao se pode criar variavel e mudar o tipo dela
+}
---
+++ src/aula_02_tipos_de_dados/02_tipos_de_dados.kt
@@ -0,0 +1,2 @@
+package aula_02_tipos_de_dados
+
