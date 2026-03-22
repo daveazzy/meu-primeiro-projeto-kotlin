46b9d3f730aeaef5f549111baa25960699536a25
daveazzy <davidsonoliveira47@gmail.com>
2026-03-22 11:26:15 -0300

    classes abertas

--- src/aula_17_classes/17_aula_3_companion_objects.kt
+++ src/aula_17_classes/17_aula_3_companion_objects.kt
@@ -1,10 +1,10 @@
 package aula_17_classes
 // uma forma de se definir membros estáticos para uma classe que nao vao pertencer às instancias, mas sim à classe
 
-class Person {
+class Person3 {
     companion object {
         const val KEY = "12345678"
-        private val KEY_2 = "1234567890"
+        private const val KEY_2 = "1234567890"
 
         fun getKey(): String {
             return KEY + KEY_2
@@ -19,13 +19,13 @@ class Person {
 }
 
 fun main() {
-    val key = Person.KEY
+    val key = Person3.KEY
 
-    val jose = Person()
-    jose.KEY // -> nao consigo acessar dessa maneira
+    val jose = Person3()
+    //jose.KEY // -> nao consigo acessar dessa maneira
 
     // nao eh possivel acessar diretamente a KEY_2
-    val key2 = Person.KEY_2
+    // val key2 = Person3.KEY_2
 
-    val obtainKey = Person.getKey()
+    val obtainKey = Person3.getKey()
 }
---
+++ src/aula_17_classes/17_aula_4_classes_abertas.kt
@@ -0,0 +1,26 @@
+package aula_17_classes
+// como implementar hierarquias dentro de uma classe
+
+// para ser extendida para outras classes, precisa ter o nome open
+open class Animal (val name: String = "Desconhecido") {
+    open val age: Int = 0
+
+    open fun sound() {
+        println("$name is sound")
+    }
+}
+
+class Dog(override val age: Int): Animal (name = "Cachorro") {
+
+
+    override fun sound  () {
+        println("$name is barking")
+    }
+}
+
+fun main() {
+    val dog = Dog(5)
+
+    dog.sound()
+    println(dog.age)
+}
