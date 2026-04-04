e0686a441136a224b4eb0745c1fd89babfb7cb4e
daveazzy <davidsonoliveira47@gmail.com>
2026-04-04 12:12:57 -0300

    today

--- src/exercicios/02_null_safety/exe_10.kt
+++ src/exercicios/02_null_safety/exe_10.kt
@@ -1,11 +1,15 @@
 package exercicios.`02_null_safety`
 
-/*
 fun converterParaInt (obj: Any): Int? {
-
+    return obj as? Int
 }
-*/
 
 fun main () {
+    val valorTexto: Any = "10"
+    val valorNumero: Any = 100
+    val valorBoleano: Any = true
 
+    println(converterParaInt(valorTexto))
+    println(converterParaInt(valorNumero))
+    println(converterParaInt(valorBoleano))
 }
