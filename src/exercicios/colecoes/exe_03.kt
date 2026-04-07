fac9dd2dfde39646687dd496252e7f21ecb99ecd
daveazzy <davidsonoliveira47@gmail.com>
2026-04-06 23:32:01 -0300

    arrays

---
+++ src/exercicios/colecoes/exe_03.kt
@@ -0,0 +1,21 @@
+package exercicios.colecoes
+
+fun main () {
+    val array: Array<Int> = arrayOf(5, 3, 8, 1, 9, 2)
+
+    for (j in 0 .. array.size) {
+        for (i in 0.. array.size -2) {
+            if (array[i] > array[i + 1]) {
+                val aux = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = aux
+            }
+        }
+    }
+
+    println(array.joinToString(
+        prefix = "{",
+        postfix = "}",
+        separator = ", ",
+    ))
+}
