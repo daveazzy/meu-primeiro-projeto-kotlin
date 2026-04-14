81afe9bce849a288e0c7f94fd9747e108453a091
daveazzy <davidsonoliveira47@gmail.com>
2026-04-13 22:32:59 -0300

    daily

--- src/exercicios/colecoes/exe_06.kt
+++ src/exercicios/colecoes/exe_06.kt
@@ -1,5 +1,12 @@
 package exercicios.colecoes
 
 fun main () {
+    val list: List<Int> = listOf(3, 7, 2, 9, 1, 5, 8, 4, 6)
 
+    println(list.find { it > 5 })
+    println(list.findLast { it < 5})
+
+    println(list.all { it % 2 == 0})
+    println(list.any { it > 8})
+    println(list.count { it > 4})
 }
