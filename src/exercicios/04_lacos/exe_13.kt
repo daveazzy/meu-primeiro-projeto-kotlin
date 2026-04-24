e60350ec8f0a7b94990cc623bb06a43d7cca670c
daveazzy <davidsonoliveira47@gmail.com>
2026-04-24 20:23:56 -0300

    today

--- src/exercicios/04_lacos/exe_13.kt
+++ src/exercicios/04_lacos/exe_13.kt
@@ -1,5 +1,12 @@
 package exercicios.`04_lacos`
 
 fun main () {
+    val lista = listOf(5, 3, 8, 12, 1, 7, 15, 4)
 
+    for ((index, i) in lista.withIndex()) {
+        if (i == 12) {
+            println("$index is ${lista[index]}")
+            break
+        }
+    }
 }
---
+++ src/exercicios/04_lacos/exe_14.kt
@@ -0,0 +1,9 @@
+package exercicios.`04_lacos`
+
+fun main () {
+    for (i in 1..10) {
+        for (j in 1..10){
+            println("$i x $j = ${i*j}")
+        }
+    }
+}
