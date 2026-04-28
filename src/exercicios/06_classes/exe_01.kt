ac7c7c5ca0582b09599640c9f36bdbf8418da749
daveazzy <davidsonoliveira47@gmail.com>
2026-04-28 13:16:34 -0300

    classes em kotlin eh diferente de classes em javascript

--- src/exercicios/06_classes/exe_01.kt
+++ src/exercicios/06_classes/exe_01.kt
@@ -1,2 +1,29 @@
 package exercicios.`06_classes`
 
+class ContaBancaria (val titular: String, private var saldo: Double) {
+    init {
+        require(saldo >= 0) { "Saldo não pode ser negativo." }
+    }
+
+    fun depositar(valor: Double) {
+        saldo += valor
+    }
+
+    fun sacar(valor: Double) {
+        if (saldo >- valor) saldo -= valor
+        else println("Saldo insuficiente.")
+    }
+
+    fun extrato  () {
+        println("Titular: $titular")
+        println("Saldo: $saldo")
+    }
+}
+
+fun main () {
+    val dave = ContaBancaria("Dave", 42.0)
+    dave.depositar(5.0)
+    dave.sacar(10.0)
+
+    dave.extrato()
+}
